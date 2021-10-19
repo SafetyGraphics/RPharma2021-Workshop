6a959596cbe928558a9c0e1e2358eee2b435418d
jwildfire <jwildfire@gmail.com>
2021-10-18 21:23:17 -0400

    update solution

--- 04_Solutions.R
+++ 04_Solutions.R
@@ -9,35 +9,42 @@ purrr::map(function(chart){
 safetyGraphicsApp(charts=allCharts)
 
 # Exercise 4.2 Solution - Convert the following static chart to run in the safetyGraphics app. Make sure that it works when custom data sets and mappings are provided.
+# Bonus - Normalize the data and plot the results using a standardized y-axis! 
+library(safetyGraphics)
+library(tidyverse)
 
 labsBoxplot <- function(data,settings){
-    ggplot(
-	    data = data, 
-	    aes(x = .data[[settings$measure_col]], y = .data[[settings$value_col]])
-    ) +
-    geom_boxplot(fill =‘blue’) +
+  p<-ggplot(
+    data = data, 
+    aes(x = .data[[settings$measure_col]], y = .data[[settings$value_col]])
+  ) +
+    geom_boxplot(fill ='blue') +
     scale_y_log10() +
     theme_bw() +
     theme(
-        axis.text.x = element_text(angle = 25, hjust = 1), 	
-        axis.text = element_text(size = 12), 
-        axis.title = element_text(size = 12)
+      axis.text.x = element_text(angle = 25, hjust = 1), 	
+      axis.text = element_text(size = 12), 
+      axis.title = element_text(size = 12)
     )
+  return(p)
 }
 
 labBoxplotConfig<-list(
-    env="safetyGraphics",
-    name="labBoxPlot",
-    label="Lab Box Plot",
-    type="plot",
-    domain="labs",
-    workflow=list(
-        main="labsBoxplot"
-    )
+  env="safetyGraphics",
+  name="labBoxPlot",
+  label="Lab Box Plot",
+  type="plot",
+  domain="labs",
+  workflow=list(
+    main="labsBoxplot"
+  )
 )
 
-charts<-list(prepareChart(labBoxplotConfig))
-safetyGraphicsApp(charts=charts, data=list(labs=safetyData::adam_adlbc))
+charts<-list(labsBoxplot=prepareChart(labBoxplotConfig))
+safetyGraphicsApp(
+  charts=charts, 
+  domainData=list(labs=safetyData::adam_adlbc)
+)
 
 # Also confirm that the chart runs for: 
 
