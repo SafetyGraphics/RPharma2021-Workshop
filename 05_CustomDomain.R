761d2935d2c8b3324b13834120c032b125f4fb5f
Jeremy Wildfire <jwildfire@gmail.com>
2021-10-22 09:03:24 -0400

    more domain examples

--- 05_CustomDomain.R
+++ 05_CustomDomain.R
@@ -19,7 +19,9 @@ sdtm <- list(
 
 safetyGraphics::safetyGraphicsApp(domainData=sdtm, meta=newMeta)
 
-# Example 5.2 - Add brand new Hello World data domain ... with an awesome custom chart
+# Example 5.2 - Add brand new Hello World data domain ... with an awesome custom chart!!
+library(safetyGraphics)
+library(tidyverse)
 
 helloMeta <- tribble(
   ~text_key, ~domain, ~label,       ~standard_hello, ~description,
@@ -39,7 +41,6 @@ helloWorld <- function(data, settings){
     text(data[[settings$x_col]], data[[settings$y_col]], "Custom Hello Domain!")
 }
 
-# Chart Configuration
 helloChart<-prepareChart(
     list(
         env="safetyGraphics",
@@ -59,11 +60,35 @@ safetyGraphicsApp(
     charts=list(helloChart)
 )
 
-# Example 5.3 - Recreate the safetyOutlierExplorer Chart using ECG data
+# Example 5.3 - Define an ECG data ... and adapt an existing chart for usage there.
+# See this PR for a full implementation of the ECG domain in safetyCharts - https://github.com/SafetyGraphics/safetyCharts/pull/90
+
+library(safetyGraphics)
+library(yaml)
+library(tidyverse)
 adeg <- readr::read_csv("https://physionet.org/files/ecgcipa/1.0.0/adeg.csv?download")
-ecg_meta <- meta
-qtOutlierExplorerChart <- prepareChart('')
 
-# See this PR for a full implementation of the ECG domain in safetyCharts - 
+ecg_meta <-tibble::tribble(
+     ~text_key, ~domain,                      ~label,                               ~description, ~standard_adam, ~standard_sdtm,
+      "id_col",   "ecg",                 "ID column", "Unique subject identifier variable name.",      "USUBJID",      "USUBJID",
+   "value_col",   "ecg",              "Value column",                 "QT result variable name.",         "AVAL",     "EGSTRESN",
+ "measure_col",   "ecg",            "Measure column",                 "QT measure variable name",        "PARAM",       "EGTEST",
+"studyday_col",   "ecg",          "Study Day column",                  "Visit day variable name",          "ADY",         "EGDY",
+   "visit_col",   "ecg",              "Visit column",                      "Visit variable name",         "ATPT",        "EGTPT",
+  "visitn_col",   "ecg",       "Visit number column",               "Visit number variable name",        "ATPTN",             NA,
+  "period_col",   "ecg",             "Period column",                     "Period variable name",      "APERIOD",             NA,
+    "unit_col",   "ecg",               "Unit column",            "Unit of measure variable name",        "AVALU",     "EGSTRESU"
+) %>% mutate(
+    col_key = text_key,
+    type="column"
+)
 
+qtOutliers<-read_yaml('https://raw.githubusercontent.com/SafetyGraphics/safetyCharts/dev/inst/config/safetyOutlierExplorer.yaml') 
+qtOutliers$label <- "QT Outlier explorer"
+qtOutliers$domain <- "ecg"
 
+safetyGraphicsApp(
+    meta=ecg_meta, 
+    domainData=list(ecg=adeg), 
+    charts=list(prepareChart(qtOutliers))
+)
--- 05_Exercise.R
+++ 05_Exercise.R
@@ -1,1 +1,7 @@
-# Exercise 5.1 - Conmeds example? 
+# Exercise 5.1 - Create a concominant medications data domain and make some charts! Test it out with safetyData::sdtm_cm.
+# Consider whether you can charts from safetyCharts (like the AE Explorer or timeline), other pacakges (https://github.com/elong0527/forestly) or build custom reports. 
+# Finalize a solution in https://github.com/SafetyGraphics/safetyCharts/pull/91
+
+library(safetyGraphics)
+library(yaml)
+library(tidyverse)
